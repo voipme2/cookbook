a39bccd063104086741b6551600bdb66f57b2eb5
Pat Laughlin <voipme2@gmail.com>
2013-05-20 21:57:39 -0400

    Very basic framework for the app.

---
+++ model/db.js
@@ -0,0 +1,35 @@
+﻿/**
+ * db.js - initial cut of the model classes used for the cookbook
+ */ 
+ 
+var mongoose = require( 'mongoose' );
+
+var recipeSchema = new mongoose.Schema({
+    name: String,
+    imageUrl: String,
+    prepTime: Number,
+    cookTime: Number,
+    tags: [String],
+    ingredients: [{
+        name: String,       // ingredient name
+        measure: String,    // tbsp, tsp, pinch
+        amount: Number      
+    }], 
+    notes: [{
+        author: String,
+        message: String
+    }],
+    steps: [{
+        directions: String,
+        imageUrl: String
+    }],
+    copyright: {
+        holder: String,
+        date: Date
+    }
+});
+
+mongoose.model( 'Recipe', recipeSchema );
+
+mongoose.connect( 'mongodb://localhost/cookbook' );
+
