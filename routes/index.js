122b41e2700682ca4a97f4488fb554fa2c227ce1
voipme2 <voipme2@gmail.com>
2013-05-27 14:27:07 -0400

    Application starts, and renders a simple page.

--- routes/index.js
+++ routes/index.js
@@ -3,17 +3,9 @@
  * GET home page.
  */
 
-var recipes = require('../model/recipes');
-
-exports.index = function(req, res){
- var sample = "Super Garlic Bread";
- recipes.recipelist(sample, function(err, rlist) {
-  res.render('index', {
-   title: 'cookbook',
-   pagetitle: 'Hello there',
-   group: strGroup,
-   teams: teamlist
+exports.index = function(req, res) {
+ res.render('index', {
+   title: 'cookbook'
   });
- });
 };
 
