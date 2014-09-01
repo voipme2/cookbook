8ce6d47e30d16a163fb9d6080d521d71594208d2
voipme2 <voipme2@gmail.com>
2014-09-01 11:48:57 -0400

    Updating

--- js/app.js
+++ js/app.js
@@ -1,71 +1,71 @@
 angular.module('cookbook', [
-	'ui.bootstrap', 
-	'ui.router',
-	'ngResource', 
-	'cookbook.services', 
-	'cookbook.controllers'])
-.config(function($stateProvider, $urlRouterProvider) {
+    'ui.bootstrap',
+    'ui.router',
+    'ngResource',
+    'cookbook.services',
+    'cookbook.controllers'])
+    .config(function ($stateProvider, $urlRouterProvider) {
 
-  $stateProvider.state('recipes', {
-  	url: "/recipes",
-  	resolve: {
-  		recipes: ['Recipe', function(Recipe) {
-  			return Recipe.query();
-  		}]
-  	},
-  	controller: function($scope, recipes) {
-      console.log("recipes: ", recipes);
-  		$scope.recipes = recipes;
-	 }
-  })
-  .state('recipes.detail', {
-  	url: "/:id",
-  	templateUrl: "partials/show.html",
-  	controller: function($scope, $stateParams) {
-  		$scope.recipe = $scope.recipes[$stateParams.id];
-  	}
-  })
-  .state("recipes.add", {
-  	url: "/add",
-  	onEnter: ['$stateParams', '$state', '$modal', 'Recipe', function($stateParams, $state, $modal, Recipe ) {
-  		var rModal = $modal.open(
-  		{
-  			templateUrl: 'partials/new-recipe.html',
-  			controller: 'NewRecipeCtrl',
-  			size: 'lg'
-  		}
-  		);
+        $stateProvider.state('recipes', {
+            url: "/recipes",
+            resolve: {
+                recipes: ['Recipe', function (Recipe) {
+                    return Recipe.query();
+                }]
+            },
+            controller: function ($scope, recipes) {
+                console.log("recipes: ", recipes);
+                $scope.recipes = recipes;
+            }
+        })
+            .state('recipes.detail', {
+                url: "/:id",
+                templateUrl: "partials/show.html",
+                controller: function ($scope, $stateParams) {
+                    $scope.recipe = $scope.recipes[$stateParams.id];
+                }
+            })
+            .state("recipes.add", {
+                url: "/add",
+                onEnter: ['$stateParams', '$state', '$modal', 'Recipe', function ($stateParams, $state, $modal, Recipe) {
+                    var rModal = $modal.open(
+                        {
+                            templateUrl: 'partials/new-recipe.html',
+                            controller: 'NewRecipeCtrl',
+                            size: 'lg'
+                        }
+                    );
 
-  		rModal.result.then(function(recipe) {
-  			var newRecipe = new Recipe(recipe);
-  			newRecipe.$save();
-  			$state.go('recipes.list');
-  		}, function() {
-				// cancel.
-			});
-  	}]
-  })
-  .state("recipes.download", {
-  	url: "/download",
-  	onEnter: ['$stateParams', '$state', '$modal', 'Recipe', function($stateParams, $state, $modal, Recipe ) {
-  		var rModal = $modal.open(
-  		{
-  			templateUrl: 'partials/download-recipe.html',
-  			controller: 'DownloadRecipeCtrl',
-  			size: 'lg'
-  		}
-  		);
+                    rModal.result.then(function (recipe) {
+                        var newRecipe = new Recipe(recipe);
+                        newRecipe.$save();
+                        $state.go('recipes.list');
+                    }, function () {
+                        // cancel.
+                    });
+                }]
+            })
+            .state("recipes.download", {
+                url: "/download",
+                onEnter: ['$stateParams', '$state', '$modal', 'Recipe', function ($stateParams, $state, $modal, Recipe) {
+                    var rModal = $modal.open(
+                        {
+                            templateUrl: 'partials/download-recipe.html',
+                            controller: 'DownloadRecipeCtrl',
+                            size: 'lg'
+                        }
+                    );
 
-  		rModal.result.then(function(recipe) {
-				// the recipe passed in is the newly downloaded recipe.
-				var newRecipe = new Recipe(recipe);
-				newRecipe.$save(function() {
-					$state.go("recipes.list");
-				})
-			}, function() {
-				// cancel.
-			});
-  	}]
-  });
+                    rModal.result.then(function (recipe) {
+                        // the recipe passed in is the newly downloaded recipe.
+                        var newRecipe = new Recipe(recipe);
+                        newRecipe.$save(function () {
+                            $state.go("recipes.list");
+                        })
+                    }, function () {
+                        // cancel.
+                    });
+                }]
+            });
 
-});
+    });
