f2199678662ca785a5f83fbabd27a020636141b3
voipme2 <voipme2@gmail.com>
2016-08-24 22:00:04 -0400

    simplified the scraper immensely

--- scraper.js
+++ scraper.js
@@ -1,53 +1,54 @@
-var Nightmare = require('nightmare');
-var nightmare = Nightmare({show: false});
-
+var request = require('request'),
+    jsdom = require('jsdom');
 
 var fetchFoodNetwork = function (recipeUrl, success, error) {
-    nightmare
-        .useragent("Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36")
-        .goto(recipeUrl)
-        .wait()
-        .evaluate(function () {
+    var opts = {
+        url: recipeUrl,
+        headers: {
+            'User-Agent': "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36"
+        }
+    };
 
+    request(opts, function (err, resp, body) {
+        if (!err && resp.statusCode === 200) {
+            jsdom.env(body, function (err, window) {
+                var document = window.document;
 
-            var recipeData = {
-                name: document.querySelector("div.title h1").textContent,
-                author: document.querySelector("div.lead-overview .media h6").textContent.replace("Recipe courtesy of ", ""),
-                servings: document.querySelector("div.difficulty > dl > dd").textContent,
-                ingredients: Array.prototype.slice.call(document.querySelectorAll("div.ingredients li .box-block")).map(function (ing) {
-                    return {text: ing.textContent};
-                }),
-                steps: Array.prototype.slice.call(document.querySelectorAll("ul.recipe-directions-list > li > p")).map(function (step) {
-                    return {text: step.textContent};
-                })
-            };
+                var recipeData = {
+                    name: document.querySelector("div.title h1").textContent,
+                    author: document.querySelector("div.lead-overview .media h6").textContent.replace("Recipe courtesy of ", ""),
+                    servings: document.querySelector("div.difficulty > dl > dd").textContent,
+                    ingredients: Array.prototype.slice.call(document.querySelectorAll("div.ingredients li")).map(function (ing) {
+                        return {text: ing.textContent};
+                    }),
+                    steps: Array.prototype.slice.call(document.querySelectorAll("ul.recipe-directions-list > li > p")).map(function (step) {
+                        return {text: step.textContent};
+                    })
+                };
 
-            var timeLabels = Array.prototype.slice.call(document.querySelectorAll("div.cooking-times > dl > dt"))
-                .slice(1).map(function (n) {
-                    return n.textContent.replace(":", "");
+                var timeLabels = Array.prototype.slice.call(document.querySelectorAll("div.cooking-times > dl > dt"))
+                    .slice(1).map(function (n) {
+                        return n.textContent.replace(":", "");
+                    });
+                var timeValues = Array.prototype.slice.call(document.querySelectorAll("div.cooking-times > dl > dd"))
+                    .slice(1).map(function (n) {
+                        return n.textContent.replace(":", "");
+                    });
+
+                timeLabels.forEach(function (l, i) {
+                    // TODO convert the times to minutes
+                    if (l != "Total time") {
+                        recipeData[l.toLowerCase() + "Time"] = timeValues[i];
+                    }
                 });
-            var timeValues = Array.prototype.slice.call(document.querySelectorAll("div.cooking-times > dl > dd"))
-                .slice(1).map(function (n) {
-                    return n.textContent.replace(":", "");
-                });
 
-            timeLabels.forEach(function (l, i) {
-                // TODO convert the times to minutes
-                if (l != "Total time") {
-                    recipeData[l.toLowerCase() + "Time"] = timeValues[i];
-                }
-            });
+                success(recipeData);
 
-            return recipeData;
-        })
-        .end()
-        .then(function (result) {
-            success(result);
-        })
-        .catch(function (err) {
-            console.error("Unable to get recipe from " + recipeUrl + ":", err);
+            })
+        } else {
             error(err);
-        });
+        }
+    });
 };
 
 module.exports = {
