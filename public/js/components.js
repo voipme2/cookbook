6e66e51dd127c2ff1ebce0ca988109adbf0d5272
Pat Laughlin <voipme2@users.noreply.github.com>
2016-12-05 15:20:12 -0500

    adding a component for search

---
+++ public/js/components.js
@@ -0,0 +1,12 @@
+function SearchController() {
+
+}
+
+angular.module("cookbook.components")
+  .component("searchBox", {
+    templateUrl: "partials/search-box.html",
+    controller: SearchController,
+    bindings: {
+    
+    }
+  });
