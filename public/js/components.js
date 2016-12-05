5925e8c0a28bcea434b6b6dec282cd3618edfb75
Pat Laughlin <voipme2@users.noreply.github.com>
2016-12-05 15:26:54 -0500

    Update components.js

--- public/js/components.js
+++ public/js/components.js
@@ -1,5 +1,20 @@
 function SearchController() {
+  var ctrl = this;
+  
+  ctrl.selectRecipe = function (item, model, label) {
+    ctrl.onSelect({id: model.id});
+  };
 
+  ctrl.getRecipes = function (search) {
+    return $http.get('/api/search', 
+      {
+        params: { query: search }
+      }).then(function (response) {
+        return response.data.sort(function (a, b) {
+          return (a.name < b.name) ? -1 : (a.name > b.name) ? 1 : 0;
+        });
+      });
+  }
 }
 
 angular.module("cookbook.components")
@@ -7,6 +22,6 @@ angular.module("cookbook.components")
     templateUrl: "partials/search-box.html",
     controller: SearchController,
     bindings: {
-    
+      onSelect: '&'
     }
   });
