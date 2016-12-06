291fc5340586dacdb79371c39a21a08a47bd752e
Pat Laughlin <voipme2@users.noreply.github.com>
2016-12-06 10:11:28 -0500

    adding in the topbar component

--- public/js/components.js
+++ public/js/components.js
@@ -1,28 +1,53 @@
-function SearchController($http) {
-    var ctrl = this;
-
-    ctrl.selectRecipe = function (item, model, label) {
-        ctrl.onSelect({ recipe: model });
-    };
-
-    ctrl.getRecipes = function (search) {
-        return $http.get('/api/search',
-            {
-                params: {query: search}
-            }).then(function (response) {
-            return response.data.sort(function (a, b) {
-                return (a.name < b.name) ? -1 : (a.name > b.name) ? 1 : 0;
-            });
-        });
-    }
-}
-
 angular.module("cookbook.components", [])
     .component("searchBox", {
         templateUrl: "partials/search-box.html",
-        controller: SearchController,
+        controller: ($http) {
+            var ctrl = this;
+
+            ctrl.selectRecipe = function (item, model, label) {
+                ctrl.onSelect({ recipe: model });
+            };
+
+            ctrl.getRecipes = function (search) {
+                return $http.get('/api/search',
+                    {
+                        params: {query: search}
+                    }).then(function (response) {
+                    return response.data.sort(function (a, b) {
+                        return (a.name < b.name) ? -1 : (a.name > b.name) ? 1 : 0;
+                    });
+                });
+            }
+        },
         controllerAs: "$ctrl",
         bindings: {
             onSelect: '&'
         }
-    });
+    })
+    .component("topBar", {
+        templateUrl: "partials/top-bar.html",
+        controller: ($http) {
+            var ctrl = this;
+
+            ctrl.selectRecipe = function (item, model, label) {
+                ctrl.onSelect({ recipe: model });
+            };
+
+            ctrl.createRecipe = function (search) {
+                return $http.get('/api/search',
+                    {
+                        params: {query: search}
+                    }).then(function (response) {
+                    return response.data.sort(function (a, b) {
+                        return (a.name < b.name) ? -1 : (a.name > b.name) ? 1 : 0;
+                    });
+                });
+            }
+        },
+        controllerAs: "$ctrl",
+        bindings: {
+            onCreate: '&',
+            onDownload: '&',
+            onSelect: '&'
+        }
+    })
