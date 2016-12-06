bd5c3e3aefc5cc8cbda8345b4c16d1fb51bf329d
Pat Laughlin <voipme2@users.noreply.github.com>
2016-12-06 10:34:14 -0500

    removing the search filter

--- public/js/filters.js
+++ public/js/filters.js
@@ -1,14 +1,4 @@
 angular.module("cookbook.filters", [])
-    .filter("recipeSearch", function () {
-        return function (recipes, search) {
-            var re = new RegExp(search, "i");
-            return recipes.filter(function (r) {
-                return r.name.match(re) || r.author.match(re) || r.ingredients.some(function (i) {
-                    return i.text.match(re);
-                });
-            });
-        }
-    })
     .filter("asTime", function () {
         return function (timeInMin) {
             var hours = parseInt(timeInMin / 60) % 24;
