726b775ba43955214cc5b3abccc4146dd33ac90a
voipme2 <voipme2@gmail.com>
2016-12-05 22:07:25 -0500

    search component works as expected

--- public/js/components.js
+++ public/js/components.js
@@ -1,27 +1,28 @@
-function SearchController() {
-  var ctrl = this;
-  
-  ctrl.selectRecipe = function (item, model, label) {
-    ctrl.onSelect({id: model.id});
-  };
+function SearchController($http) {
+    var ctrl = this;
 
-  ctrl.getRecipes = function (search) {
-    return $http.get('/api/search', 
-      {
-        params: { query: search }
-      }).then(function (response) {
-        return response.data.sort(function (a, b) {
-          return (a.name < b.name) ? -1 : (a.name > b.name) ? 1 : 0;
+    ctrl.selectRecipe = function (item, model, label) {
+        ctrl.onSelect({ recipe: model });
+    };
+
+    ctrl.getRecipes = function (search) {
+        return $http.get('/api/search',
+            {
+                params: {query: search}
+            }).then(function (response) {
+            return response.data.sort(function (a, b) {
+                return (a.name < b.name) ? -1 : (a.name > b.name) ? 1 : 0;
+            });
         });
-      });
-  }
+    }
 }
 
-angular.module("cookbook.components")
-  .component("searchBox", {
-    templateUrl: "partials/search-box.html",
-    controller: SearchController,
-    bindings: {
-      onSelect: '&'
-    }
-  });
+angular.module("cookbook.components", [])
+    .component("searchBox", {
+        templateUrl: "partials/search-box.html",
+        controller: SearchController,
+        controllerAs: "$ctrl",
+        bindings: {
+            onSelect: '&'
+        }
+    });
