3993216ccab6842f55b5e01031328098760919e3
voipme2 <voipme2@gmail.com>
2018-10-15 22:05:10 -0400

    fixed foodnetwork parser

--- server/scraper.js
+++ server/scraper.js
@@ -1,105 +1,91 @@
 var request = require('request'),
-    jsdom = require('jsdom'),
-    moment = require('moment'),
-    parseDuration = require('parse-duration');
+  jsdom = require('jsdom'),
+  moment = require('moment'),
+  parseDuration = require('parse-duration');
 
 const JSDOM = jsdom.JSDOM;
 const HEADERS = {
-    'User-Agent': "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36"
+  'User-Agent': "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36"
 };
 
 var fetchAllRecipes = function (recipeUrl, success, error) {
-    request({url: recipeUrl, headers: HEADERS}, function (err, resp, body) {
-        if (!err && resp.statusCode === 200) {
-            var env = new JSDOM(body);
-            var document = env.window.document;
-            var sNumber = document.querySelector("meta[itemprop='recipeYield']").content;
-            var recipeData = {
-                name: document.querySelector("h1.recipe-summary__h1").textContent,
-                author: document.querySelector("span.submitter__name").textContent,
-                servings: sNumber + " " + document.querySelector("span.servings-count").textContent.trim(),
-                ingredients: Array.prototype.slice.call(document.querySelectorAll("span.recipe-ingred_txt[itemprop='ingredients']")).map(function (ing) {
-                    return {text: ing.textContent};
-                }).filter(function (ing) {
-                    return ing.text.length > 0 && ing.text !== "Add all ingredients to list";
-                }),
-                steps: Array.prototype.slice.call(document.querySelectorAll("span.recipe-directions__list--item")).map(function (step) {
-                    return {text: step.textContent};
-                }).filter(function (ste) {
-                    return ste.text.length > 0
-                }),
-                prepTime: moment.duration(parseDuration(document.querySelector("time[itemprop='prepTime']").textContent)).asMinutes() + " min",
-                cookTime: moment.duration(parseDuration(document.querySelector("time[itemprop='cookTime']").textContent)).asMinutes() + " min"
-            };
+  request({url: recipeUrl, headers: HEADERS}, function (err, resp, body) {
+    if (!err && resp.statusCode === 200) {
+      var env = new JSDOM(body);
+      var document = env.window.document;
+      var sNumber = document.querySelector("meta[itemprop='recipeYield']").content;
+      var recipeData = {
+        name: document.querySelector("h1.recipe-summary__h1").textContent,
+        author: document.querySelector("span.submitter__name").textContent,
+        servings: sNumber + " " + document.querySelector("span.servings-count").textContent.trim(),
+        ingredients: Array.prototype.slice.call(document.querySelectorAll("span.recipe-ingred_txt[itemprop='ingredients']")).map(function (ing) {
+          return {text: ing.textContent};
+        }).filter(function (ing) {
+          return ing.text.length > 0 && ing.text !== "Add all ingredients to list";
+        }),
+        steps: Array.prototype.slice.call(document.querySelectorAll("span.recipe-directions__list--item")).map(function (step) {
+          return {text: step.textContent};
+        }).filter(function (ste) {
+          return ste.text.length > 0
+        }),
+        prepTime: moment.duration(parseDuration(document.querySelector("time[itemprop='prepTime']").textContent)).asMinutes() + " min",
+        cookTime: moment.duration(parseDuration(document.querySelector("time[itemprop='cookTime']").textContent)).asMinutes() + " min"
+      };
 
-            success(recipeData);
+      success(recipeData);
 
-        } else {
-            error(err);
-        }
-    });
+    } else {
+      error(err);
+    }
+  });
 };
 
+function getTime(time) {
+  return moment.duration(parseDuration(time));
+}
+
 var fetchFoodNetwork = function (recipeUrl, success, error) {
-    request({url: recipeUrl, headers: HEADERS}, function (err, resp, body) {
-        if (!err && resp.statusCode === 200) {
-            var env = new JSDOM(body);
-            var document = env.window.document;
-
-            var recipeData = {
-                name: document.querySelector("span.o-AssetTitle__a-HeadlineText").textContent,
-                author: document.querySelector("span.o-Attribution__a-Name > a").textContent,
-                servings: document.querySelector("section.o-RecipeInfo.o-Yield dd.o-RecipeInfo__a-Description").textContent.trim(),
-                ingredients: Array.prototype.slice.call(document.querySelectorAll("label.o-Ingredients__a-ListItemText")).map(function (ing) {
-                    return {text: ing.textContent};
-                }),
-                steps: Array.prototype.slice.call(document.querySelectorAll("div.o-Method__m-Body > p"), 1).map(function (step) {
-                    return {text: step.textContent.trim()};
-                })
-            };
-
-            var timeLabels =  Array.prototype.slice.call(document.querySelectorAll("section.o-RecipeInfo.o-Time > dl > dt"))
-                .map(function (n) {
-                    return n.textContent.replace(":", "");
-                });
-            var timeValues = Array.prototype.slice.call(document.querySelectorAll("section.o-RecipeInfo.o-Time > dl > dd"))
-                .map(function (n) {
-                    return moment.duration(parseDuration(n.textContent.replace(":", "")));
-                });
-
-            var times = {};
-            timeLabels.forEach(function(label, ind) {
-                times[label] = timeValues[ind];
-            });
-            if (times.Active) {
-                recipeData.inactiveTime = times.Total.subtract(times.Active).asMinutes() + " min";
-                recipeData.cookTime = times.Active.asMinutes() + " min";
-            } else {
-                if (times.Prep) {
-                    recipeData.prepTime = times.Prep.asMinutes() + " min";
-                }
-                if (times.Cook) {
-                    recipeData.cookTime = times.Cook.asMinutes() + " min";
-                }
-            }
-
-            success(recipeData);
-        } else {
-            error(err);
-        }
-    });
+  request({url: recipeUrl, headers: HEADERS}, function (err, resp, body) {
+    if (!err && resp.statusCode === 200) {
+      var env = new JSDOM(body);
+      var document = env.window.document;
+      // thanks, foodnetwork!
+      var ldData = JSON.parse(document.querySelector("script[type='application/ld+json']").textContent);
+      var recipeData = {
+        name: ldData.name,
+        author: ldData.author[0].name,
+        servings: ldData.recipeYield,
+        ingredients: ldData.recipeIngredient.map(function (ing) {
+          return {text: ing};
+        }),
+        steps: ldData.recipeInstructions.map(function (step) {
+          return {text: step};
+        })
+      };
+      if (ldData.prepTime) {
+        recipeData.prepTime = getTime(ldData.prepTime).asMinutes() + " min";
+      }
+      if (ldData.cookTime) {
+        recipeData.inactiveTime = getTime(ldData.totalTime).subtract(getTime(ldData.cookTime)).asMinutes() + " min";
+        recipeData.cookTime = getTime(ldData.cookTime).asMinutes() + " min";
+      }
+      success(recipeData);
+    } else {
+      error(err);
+    }
+  });
 };
 
 module.exports = {
 
-    fetch: function (url, success, error) {
-        if (url.indexOf("foodnetwork.com") !== -1) {
-            return fetchFoodNetwork(url, success, error)
-        } else if (url.indexOf("allrecipes.com") !== -1) {
-            return fetchAllRecipes(url, success, error);
-        }
-    },
+  fetch: function (url, success, error) {
+    if (url.indexOf("foodnetwork.com") !== -1) {
+      return fetchFoodNetwork(url, success, error)
+    } else if (url.indexOf("allrecipes.com") !== -1) {
+      return fetchAllRecipes(url, success, error);
+    }
+  },
 
-    fetchFoodNetwork: fetchFoodNetwork,
-    fetchAllRecipes: fetchAllRecipes
+  fetchFoodNetwork: fetchFoodNetwork,
+  fetchAllRecipes: fetchAllRecipes
 };
--- tests/fetch.spec.js
+++ tests/fetch.spec.js
@@ -2,7 +2,7 @@
 var request = require('request');
 var scraper = require('../server/scraper');
 describe("scraper tests", function() {
-    it("should return a recipe from allrecipes", function (done) {
+    it("should return overnight blueberry french toast from allrecipes", function (done) {
         scraper.fetchAllRecipes("http://allrecipes.com/recipe/15057/overnight-blueberry-french-toast/",
             function(recipe) {
                 expect(recipe.name).toEqual("Overnight Blueberry French Toast");
@@ -13,7 +13,7 @@ describe("scraper tests", function() {
         );
     });
 
-    it("should return a recipe from foodnetwork", function(done) {
+    it("should return fettucine with creamy red pepper  from foodnetwork", function(done) {
         scraper.fetchFoodNetwork("http://www.foodnetwork.com/recipes/ellie-krieger/fettuccine-with-creamy-red-pepper-feta-sauce-recipe-1946840",
             function(recipe) {
                 expect(recipe.name).toEqual("Fettuccine with Creamy Red Pepper-Feta Sauce");
@@ -24,7 +24,7 @@ describe("scraper tests", function() {
         );
     });
 
-    it("should return a recipe from foodnetwork", function(done) {
+    it("should return orecchiette with pancetta pumpkin from foodnetwork", function(done) {
         scraper.fetchFoodNetwork("http://www.foodnetwork.com/recipes/anne-burrell/orecchiette-with-pancetta-pumpkin-and-broccoli-rabe-3568201",
             function(recipe) {
                 expect(recipe.name).toEqual("Orecchiette with Pancetta, Pumpkin and Broccoli Rabe");
@@ -35,12 +35,22 @@ describe("scraper tests", function() {
         );
     });
 
-    it("should return a recipe from foodnetwork with no cooktime", function(done) {
+    it("should return veggie lovers club sandwich k with no cooktime", function(done) {
         scraper.fetchFoodNetwork("http://www.foodnetwork.com/recipes/food-network-kitchen/veggie-lovers-club-sandwich-recipe-2120987",
             function(recipe) {
                 expect(recipe.name).toEqual("Veggie Lover's Club Sandwich");
                 expect(recipe.cookTime).toBeUndefined();
                 expect(recipe.prepTime).toEqual("30 min");
+                done();
+            }
+        );
+    });
+    it("should return marinara sauce from foodnetwork with steps", function(done) {
+        scraper.fetchFoodNetwork("https://www.foodnetwork.com/recipes/giada-de-laurentiis/marinara-sauce-recipe-2103577",
+            function(recipe) {
+                expect(recipe.name).toEqual("Marinara Sauce");
+                expect(recipe.cookTime).toEqual("70 min");
+                expect(recipe.prepTime).toEqual("10 min");
                 done();
             }
         );
